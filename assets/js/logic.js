d3a75c1f8cda6f42c272b6f8ab70af833c4650f5
Glutey <mr_tscreen@hotmail.com>
2023-01-24 23:49:42 +0000

    All code from old repo added

---
+++ assets/js/logic.js
@@ -0,0 +1,109 @@
+//  To begin task we must declare all variables first;  so make a list (hints are left within the HTML for reference) 
+
+var time = 60;
+var timer;
+var currentQuestion = 0;
+var score = 0;
+var submit = ;
+
+// sound effects for right and wrong answer
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+                
+ // start game with an event listener - set up function to start timer and display questions.
+ var startQuiz = document.getElementById("start"); 
+ document.addEventListener("click"); // this should then be startQuiz.addEventListener("click"); this then states what we want from it
+
+ var questionContainer = document.getElementById("questons"); //used to grab div for the Questions.
+ var questionsTitle = document.getElementById("question-title");
+ var choicesContainer = document.getElementById("choices"); // Grab div for the answer choices.
+ var endScreen = document.getElementById("end-screen");
+ var finalScore = document.getElementById("final-score");
+ var initialsInput = document.getElementById("initials");
+ var submitButton = document.getElementById("submit");
+ submitButton.addEventListener("click", saveScore);
+
+ var endQuiz = document.getElementById("end-screen"); // Grabs the div that will allow us to end the game and toggle the visibility.
+ 
+ var feedback = document.getElementById("feedback");
+
+
+// Functions for working code:
+
+
+function startQuiz() {
+  startButton.classList.add("hide");
+  questionsContainer.classList.remove("hide");
+  timer = setInterval(function() {
+    time--;
+    document.getElementById("time").innerHTML = time;
+    if (time === 0) {
+      clearInterval(timer);
+      endQuiz(); // this is a function built at the end of the code, and it is just called here
+    }
+  }, 1000);
+  displayQuestion();
+}
+
+function displayQuestion() {
+  if (currentQuestion < questions.length) {
+    questionTitle.innerHTML = questions[currentQuestion].question;
+    choicesContainer.innerHTML = "";
+    for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+      var choice = document.createElement("button");
+      choice.innerHTML = questions[currentQuestion].choices[i];
+      choice.addEventListener("click", checkAnswer);
+      choicesContainer.appendChild(choice);
+    }
+  } else {
+    endQuiz();
+  }
+}
+
+
+  // function displayQuestion() {
+  //     var currentQuest = questions[currentQuestion];//create a variable to get the current question object from question array
+  //     if (currentQuest < questions.length){
+  //       questionTitle.innerHTML = questions[currentQuestion].question; //update title with with current question...currentQuest.title
+  //       choicesContainer.innerHTML = "";
+  //       for (var i = 0; i < questions[currentQuestion].choices.length; i++) { //loop over questions
+  //         var choice = document.createElement("button"); //create a new button for each choice
+  //         choice.innerHTML = questions[currentQuestion].choices[i]; //set a value attribute to each with a value of questions.choices[i]
+  //         choice.addEventListener("click", checkAnswer); //attach a click event to the button with the checkAnswer function as the event
+  //         choicesContainer.appendChild(choice); //append the button to the choices
+  //       }
+  //     } else {
+  //       endQuiz(); //clear out the choices element
+  //     }
+  //   }
+    
+  
+  function checkAnswer() {
+    if (this.innerHTML === questions[currentQuestion].answer) { //check if the value of the button clicked(this) and if it matches the answer of the current question
+      //then make the feedback element have a text of 'Correct'
+      score++;
+      feedback.innerHTML = sfxRight();
+      feedback.classList.remove("hide");
+    } else {
+      time -= 10; //else you need to reduce 15 seconds from the time
+      feedback.innerHTML = sfxWrong;
+      feedback.classList.remove("hide"); //remove the class of hide to the feedback element
+      timeLeft(); //display the new time on the page
+    }
+    setTimeout(function() { //set a timeout function so that the class of hide gets added back after a second to hide the feedback
+      feedback.classList.add("hide");
+      currentQuestion++;
+      displayQuestion(); //move to the next question
+    }, 1000);
+  }
+  
+    //make a condition so that if we ran out of questions the quiz ends, else displayQuestion
+
+  //create a function to end the quiz
+function endQuiz() {
+  clearInterval(timer); //inside the function clear the interval of the timer
+  questionsContainer.classList.add("hide"); //hide the questions
+  endScreen.classList.remove //show the end screen
+
+
+  //show the final score
---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        question: "What does DOM stand for?",
+        choices: ["Domain Obsessive Monitor", "Double Object Model", "Document Object Model"],
+        answer: "Document Object Model" 
+    },
+    {
+        question: "What does Const do? ",
+        choices: ["Const allows the varible to change its value", "Const makes the code 'considerate' and in keeping with accessibility", "Const ensures that the variable is unchangable"],
+        answer: "Const ensures that the variable is unchangable" 
+    },
+    {
+        question: "what does API stand for?",
+        choices: ["Application Programable Interface", "Application Progress Interface", "Applicable Program Intro"],
+        answer: "Application Programable Interface" 
+    },
+    {
+        question: "What how can you add information to an array?",
+        choices: [".new-item()", ".add()", ".append()"],
+        answer: ".append()" 
+    },
+    {
+        question: "What information type returns either true or false?",
+        choices: ["boolean", "string", "number"],
+        answer: "Application Programable Interface" 
+    },
+    {
+        question: "Which operator is 'strict' equals?",
+        choices: ["=", "===", "=="],
+        answer: "===" 
+    }
+];
+
+
+
+
+   
+// below is super brief pseudo code for the challenge requirements.
+// Timer = set limit, count down 1000 ms each time, subtrack 10 seconds for each wrong answer, sound affect for right or wrong answer, game ends when timer reaches 0 or when all questions are answered
